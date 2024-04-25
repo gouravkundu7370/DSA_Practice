8785044e03d2ebca029d20f5a8f85180f7b9c19c
Gourav Kundu <gouravkundu2902@gmail.com>
2024-04-26 02:46:17 +0530

    Added Dynamic Problem approach and logical Approach

--- Arrays/src/UniquePaths.java
+++ Arrays/src/UniquePaths.java
@@ -2,25 +2,39 @@ public class UniquePaths {
 
     public static void main(String[] args) {
 
-        System.out.println(uniquePaths(2, 3));
+        System.out.println(uniquePaths(3, 7));
+        System.out.println(uniquePaths(4,4));
     }
 
     
-    public static int uniquePaths(int m, int n) {
+    public static int uniquePaths1(int m, int n) {
         int ans = countPaths(0, 0, m, n);
         return ans;
            
     }
 
 // Recursive
-    private static int countPaths(int i, int j, int m, int n) {
-        if (i == m - 1 && j == n - 1) {
-            return 1;
-        }
-        if (i >= m || j >= n) {
-            return 0;
-        }
-        else
-            return countPaths(i + 1, j, m, n) + countPaths(i, j + 1, m, n);
+private static int countPaths(int i, int j, int m, int n) {
+    if (i == m - 1 && j == n - 1) {
+        return 1;
     }
+    if (i >= m || j >= n) {
+        return 0;
+    } else
+        return countPaths(i + 1, j, m, n) + countPaths(i, j + 1, m, n);
+}
+    
+ public static int uniquePaths(int m, int n) {
+    int x = m + n - 2;
+        int r = Math.min(m, n) - 1;
+        double res = 1;
+        for (int i = 1; i <= r; i++) {
+            res = res *  (x - r + i) / i;
+
+        }
+        return (int) res;
+
+           
+    }
+
 }
